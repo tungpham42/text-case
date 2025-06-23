d67071ec13ee0f61d370ee72dee0b93e7de42905
Tung Pham <tung.42@gmail.com>
2025-06-23 19:03:28 +0700

    add brand logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Footer from "./components/Footer";
+import MainBrandLogo from "./components/MainBrandLogo";
 import TextManipulator from "./components/TextManipulator";
 import { LanguageProvider } from "./context/LanguageContext";
 
 const App = () => {
   return (
     <LanguageProvider>
+      <MainBrandLogo
+        logoSrc="/soft-logo.webp"
+        mainDomain="soft.io.vn"
+        dismissible={false}
+        altText="Logo Soft"
+      />
       <TextManipulator />
       <Footer />
     </LanguageProvider>
