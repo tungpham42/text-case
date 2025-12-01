fdd6fcf773e4d57fa7b706e3f5e5ddb472f3fabd
Tung Pham <tung.42@gmail.com>
2025-12-01 14:54:55 +0700

    mb-5

--- src/components/MainBrandLogo.js
+++ src/components/MainBrandLogo.js
@@ -14,7 +14,7 @@ const MainBrandLogo = ({
 
   return (
     <div
-      className="position-fixed top-0 start-0 m-3 d-flex align-items-center bg-white rounded shadow-sm px-2 py-1"
+      className="position-absolute top-0 start-0 mb-5 d-flex align-items-center bg-white rounded shadow-sm px-2 py-1"
       style={{ zIndex: 1050 }}
     >
       <a
